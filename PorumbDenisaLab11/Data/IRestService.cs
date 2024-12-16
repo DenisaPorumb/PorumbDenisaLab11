3899cc9d71a1bc34ecfc3cf84025795f9c00bddf
DENISA-RALUCA PORUMB <denisa.porumb@stud.ubbcluj.ro>
2024-12-16 17:39:36 +0200

    Add project files.

---
+++ PorumbDenisaLab11/Data/IRestService.cs
@@ -0,0 +1,23 @@
+﻿using PorumbDenisaLab11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorumbDenisaLab11.Data
+{
+    public interface IRestService
+    {
+        Task DeleteShopListAsync(int iD);
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+
+        public interface IRestService
+        {
+            Task<List<ShopList>> RefreshDataAsync(); 
+            Task SaveShopListAsync(ShopList item, bool isNewItem);
+            Task DeleteShopListAsync(int id); 
+        }
+    }
+}
